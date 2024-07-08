1761b9d0c7a69905417cc7dd4d1f3c4ac3347117
ultimatile <ultimatile@users.noreply.github.com>
2024-07-08 14:59:39 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # itensor_tutorial
 
 [![Build Status](https://github.com/ultimatile/itensor_tutorial.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/ultimatile/itensor_tutorial.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+## target model: $S=1/2$ transverse field Ising chain
+
+Hamiltonian:
+
+$$
+H=\sum_{i=1}^N\left(JS^z_i S^z_{i+1} + h S^x_i\right)
+$$
+
+When `periodic=true`, the periodic boundary condition is applied: $S^z_{N+1}=S^z_1$.
+When `periodic=false`, the open boundary condition is applied; the maximum of the summation index $i$ changes as $N\to N-1$.
+The default value is `periodic=true`.
+
+## exact ground state energy
+
+$$
+E = \sum_{i=1}^{N/2}
+\sqrt{\left(h-\frac12J\cos \left(\frac{2i-1}{N}\right)\right)^2+\frac{J^2}{4}\sin^2 \left(\frac{2i-1}{N}\right)}
+=\sum_{i=1}^{N/2}\sqrt{h^2-hJ\cos\left(\frac{2i-1}{N}\right)+\frac{J^2}{4}}
+$$
+
+Here, the number of spins $N$ should be even.
+
+## how to run
+
+After executing Julia REPL, activate the environment and run the script:
+
+```sh:
+Pkg> activate .
+using itensor_tutorial
+main()
+```
+
+Alternatively, you can run the script directly with your Julia environment:
+
+```sh:
+julia src/itensor_tutorial.jl
+```
+
+## expected output
+
+```
+Ground state energy (DMRG) = -3.830648787056722
+Ground state energy (analytical) = -3.8306487877701936
+
+Energy error = 7.134715040990613e-10
+```
--- src/itensor_tutorial.jl
+++ src/itensor_tutorial.jl
@@ -1,5 +1,56 @@
 module itensor_tutorial
+export main
+using ITensors, ITensorMPS
 
-# Write your package code here.
+function main()
+  # model parameters
+  N = 12 # lattice size
+  J = -1.0 # SzSz coupling strength
+  h = 0.5 # Sx fielf strengh
+  periodic = true # boundary conditions
+  #local dofs
+  sites = siteinds("S=1/2",N)
+  
+  # Operator Sum (transverse field Ising model with open boundary conditions)
+  os = OpSum()
+  for j=1:N-1
+    os += J,  "Sz", j, "Sz", j+1
+    os += h,  "Sx", j
+  end
+  if periodic
+    os += J, "Sz", N, "Sz", 1
+    os += h, "Sx", N
+  end
+  # Creating the Hamiltonian from the Operator Sum
+  H = MPO(os,sites)
+  
+  # calculation parameters
+  nsweeps = 25 # number of sweeps is 5
+  maxdim = [10, 10, 10, 10, 20, 40, 100, 100, 200, 200, 400] # gradually increase states kept
+  cutoff = [1E-10] # desired truncation error
 
+  # initial stat preparation (all spins up (ferromagnetic state))
+  psi0 = MPS(sites, "Up")
+  
+  # DMRG calculation
+  energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff)
+  println("Ground state energy (DMRG) = ", energy)
+ 
+  # calculate the exact ground state energy
+  exact_ene = 0.0
+  isodd(N) && @warn("N is odd. The output of the exact ground state energy is invalid.")
+  for i in 1:2:N-1
+    q = i * pi / N
+    #exact_ene += sqrt((h-0.5J*cos(q))^2+0.25(J*sin(q))^2) 
+    exact_ene += sqrt(h^2 + 0.25J^2 - h*J*cos(q))
+  end
+  println("Ground state energy (analytical) = ", -exact_ene)
+
+  println()
+  println("Energy error = ", abs(energy + exact_ene))
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+  main()
+end
 end
