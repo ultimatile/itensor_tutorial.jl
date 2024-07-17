bd2b26440ceb4352604f7e6bebd96560c570f85b
ultimatile <ultimatile@users.noreply.github.com>
2024-07-18 03:17:31 +0900

    implement analytic gs energy for odd spins

--- README.md
+++ README.md
@@ -14,15 +14,16 @@ When `periodic=true`, the periodic boundary condition is applied: $S^z_{N+1}=S^z
 When `periodic=false`, the open boundary condition is applied; the maximum of the summation index $i$ changes as $N\to N-1$.
 The default value is `periodic=true`.
 
-## exact ground state energy
+## exact ground state energy for periodic boundary condition
 
 $$
-E = -\sum_{i=1}^{N/2}
-\sqrt{\left(h-\frac12J\cos \left(\frac{2i-1}{N}\right)\right)^2+\frac{J^2}{4}\sin^2 \left(\frac{2i-1}{N}\right)}
-=-\sum_{i=1}^{N/2}\sqrt{h^2-hJ\cos\left(\frac{2i-1}{N}\right)+\frac{J^2}{4}}
+E = -\frac12\sum_{i=1}^{N}
+\sqrt{\left(h-\frac{J}{2}\cos q_i\right)^2+\frac{J^2}{4}\sin^2q_i}
+=-\frac12\sum_{i=1}^{N}\sqrt{h^2-hJ\cos q_i+\frac{J^2}{4}}
 $$
 
-Here, the number of spins $N$ should be even, and the periodic boundary condition is assumed.
+Here, $q_i=\frac{2\pi}{N}(i-\frac12)$ if $N$ is even, and $q_i=\frac{2\pi}{N}i$ if $N$ is odd.
+The periodic boundary condition is assumed.
 
 ## how to run
 
@@ -41,10 +42,22 @@ julia src/itensor_tutorial.jl
 ```
 
 ## expected output
+`J = -1.0`, `h = 0.5`, and `periodic = true` are used.
+
+### $N=12$
 
 ```
 Ground state energy (DMRG) = -3.830648787056722
 Ground state energy (analytical) = -3.8306487877701936
 
 Energy error = 7.134715040990613e-10
+```
+
+### $N=13$
+
+```
+Ground state energy (DMRG) = -4.148114903976182
+Ground state energy (analytical) = -4.148114905279376
+
+Energy error = 1.303193997159724e-9
 ```
--- src/itensor_tutorial.jl
+++ src/itensor_tutorial.jl
@@ -8,46 +8,54 @@ function main()
   J = -1.0 # SzSz coupling strength
   h = 0.5 # Sx fielf strengh
   periodic = true # boundary conditions
+  # calc mode
+  calc_dmrg = true 
+  #calc_dmrg = false 
+  analytic = true
+
   #local dofs
-  sites = siteinds("S=1/2",N)
-  
-  # Operator Sum (transverse field Ising model with open boundary conditions)
-  os = OpSum()
-  for j=1:N-1
-    os += J,  "Sz", j, "Sz", j+1
-    os += h,  "Sx", j
-  end
-  if periodic
-    os += J, "Sz", N, "Sz", 1
-    os += h, "Sx", N
-  end
-  # Creating the Hamiltonian from the Operator Sum
-  H = MPO(os,sites)
-  
-  # calculation parameters
-  nsweeps = 25 # number of sweeps is 5
-  maxdim = [10, 10, 10, 10, 20, 40, 100, 100, 200, 200, 400] # gradually increase states kept
-  cutoff = [1E-10] # desired truncation error
+  if calc_dmrg
+    sites = siteinds("S=1/2", N)
 
-  # initial stat preparation (all spins up (ferromagnetic state))
-  psi0 = MPS(sites, "Up")
-  
-  # DMRG calculation
-  energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff)
-  println("Ground state energy (DMRG) = ", energy)
- 
-  # calculate the exact ground state energy
-  exact_ene = 0.0
-  isodd(N) && @warn("N is odd. The output of the exact ground state energy is invalid.")
-  !periodic && @warn("The open boundary condition is applied. The output of the exact ground state energy is invalid.") 
-  for i in 1:2:N-1
-    q = i * pi / N
-    exact_ene += sqrt(h^2 + 0.25J^2 - h*J*cos(q))
-  end
-  println("Ground state energy (analytical) = ", -exact_ene)
+    # Operator Sum (transverse field Ising model with open boundary conditions)
+    os = OpSum()
+    for j = 1:N-1
+      os += J, "Sz", j, "Sz", j + 1
+      os += h, "Sx", j
+    end
+    if periodic
+      os += J, "Sz", N, "Sz", 1
+      os += h, "Sx", N
+    end
+    # Creating the Hamiltonian from the Operator Sum
+    H = MPO(os, sites)
 
-  println()
-  println("Energy error = ", abs(energy + exact_ene))
+    # calculation parameters
+    nsweeps = 25 # number of sweeps is 5
+    maxdim = [10, 10, 10, 10, 20, 40, 100, 100, 200, 200, 400] # gradually increase states kept
+    cutoff = [1E-10] # desired truncation error
+
+    # initial stat preparation (all spins up (ferromagnetic state))
+    psi0 = MPS(sites, "Up")
+
+    # DMRG calculation
+    energy, psi = dmrg(H, psi0; nsweeps, maxdim, cutoff)
+    println("Ground state energy (DMRG) = ", energy)
+  end
+  if analytic
+    # calculate the exact ground state energy
+    exact_ene = 0.0
+    !periodic && @warn("The open boundary condition is applied. The output of the exact ground state energy is invalid.")
+    for i in 1:N
+      q = isodd(N) ? 2pi / N * i : 2pi / N * (i - 0.5) 
+      exact_ene += 0.5*sqrt(h^2 + 0.25J^2 - h * J * cos(q))
+    end
+    println("Ground state energy (analytical) = ", -exact_ene)
+  end
+  if calc_dmrg && analytic
+    println()
+    println("Energy error = ", abs(energy + exact_ene))
+  end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
